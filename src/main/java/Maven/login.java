b76d01daedcbb58bd9e2eccb80e62f509a4bfd96
ram <rk5193016@gmail.com>
2024-08-15 10:34:58 +0530

    New Hi

---
+++ src/main/java/Maven/login.java
@@ -0,0 +1,18 @@
+package Maven;
+
+public class login {
+	
+	public void login() {
+		System.out.println("login");
+	}
+
+
+	public static void main(String[] args) {
+		
+		login input = new login();
+		input.login();
+		
+	}
+	
+	
+}
